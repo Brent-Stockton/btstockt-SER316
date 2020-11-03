647a38c6368491dfdb65ff2a54f6fd84e86516dd
brentstockton <brentstockton1@gmail.com>
2020-11-02 20:52:26 -0700

    defect-4 Constants and Enums should be in all Caps. Fixed PRICE_PER_LETTER to reflect coding standards

--- src/main/java/Embroidery.java
+++ src/main/java/Embroidery.java
@@ -1,12 +1,12 @@
 package main.java;
 
 public class Embroidery {
-    final double pricePerLetter = 1.00;
+    final double PRICE_PER_LETTER = 1.00;
     double price;
     String embroidText;
 
     public Embroidery (String embroidery) {
         this.embroidText = embroidery;
-        this.price = embroidery.length() * pricePerLetter;
+        this.price = embroidery.length() * PRICE_PER_LETTER;
     }
 }
